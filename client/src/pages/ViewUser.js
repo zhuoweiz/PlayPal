d98c4987c7a53f4c5723c88238ee45b53898fde5
thomasgy422 <thomasku1997@gmail.com>
2021-12-01 19:40:16 -0500

    change

--- client/src/pages/ViewUser.js
+++ client/src/pages/ViewUser.js
@@ -11,6 +11,40 @@ import { deepOrange, deepPurple } from "@mui/material/colors";
 import Paper from "@mui/material/Paper";
 import CenteredTabs  from "../components/CenteredTabs";
 import PostCardMatrix from "../components/PostCardMatrix";
+<<<<<<< Updated upstream
+=======
+import { makeStyles } from '@mui/styles';
+import { typography } from "@mui/system";
+
+const useStyles = makeStyles((theme) => ({
+  tag: {
+    marginRight: theme.spacing(1),
+  },
+  tagBox: {
+    width: "100%", 
+    marginTop: theme.spacing(1),
+  },
+}));
+
+
+const interests = [
+  {
+    value: "Game"
+  },
+  {
+    value: "Sport"
+  },
+  {
+    value: "Read"
+  },
+  {
+    value: "Coding"
+  },
+  {
+    value: "Movie"
+  },
+]
+>>>>>>> Stashed changes
 
 function ViewUser() {
   return (
@@ -55,12 +89,35 @@ function ViewUser() {
             </Button>
           </Grid>
         </Paper>
-        <Paper container item md={3} style={{marginTop:"24px" , padding:"12px"}} direction="column" component={Grid}>
-          <Grid item md={8} style={{}}>
-            bio
+        <Paper container item md={3} style={{marginTop:"24px" , padding:"12px"}} component={Grid}>
+          <Grid item md={12} style={{}}>
+            <Typography variant = "h6">
+              bio
+            </Typography>
+            <Typography>
+              Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text.
+            </Typography>
           </Grid>
+<<<<<<< Updated upstream
           <Grid item md={4} style={{}}>
             interest tags
+=======
+          <Grid item md={12} style={{}}>
+            <Typography variant = "h6">
+              interest tags
+            </Typography>
+            <div className={classes.tagBox}>
+              {
+                interests.map((element, index) => {
+                  return (
+                    <Chip 
+                      key={index}
+                      className={classes.tag} label={element.value} variant="outlined" />
+                  )
+                })
+              }
+            </div>
+>>>>>>> Stashed changes
           </Grid>
         </Paper>
 
