db633ea637a740dec7fc20a4d7260af82ff8faf4
zhuowei zhang <zhuoweiz10@gmail.com>
2021-12-04 18:06:48 -0500

    [zhuoweiz] search post by name & tag done

--- client/src/components/other/SearchPopUp.js
+++ client/src/components/other/SearchPopUp.js
@@ -7,31 +7,36 @@ import {
 } from '@mui/material';
 import { Dialog, DialogTitle, DialogContent, DialogActions} from '@mui/material';
 import { useSnackbar } from 'notistack';
+import { serverUrl } from '../../constants';
+import { useNavigate } from 'react-router-dom';
+
+const axios = require("axios");
 
 
 export default function SearchPopUp(props) {
   const { onClose, selectedValue, open } = props;
 
-  const [alignment, setAlignment] = React.useState(1);
-  const [alignment2, setAlignment2] = React.useState(1);
-  const [searchType, setSearchType] = React.useState(0);
+  const [searchType, setSearchType] = React.useState(1);
+  const [searchRule, setSearchRule] = React.useState(1);
   const [searchValue, setSearchValue] = React.useState("");
+  const [searchResult, setSearchResult] = React.useState([]);
 
   const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
 
   const handleAlignment = (event, newAlignment) => {
-    if(alignment2 === 2 && newAlignment === 2) {
+    if(searchRule === 2 && newAlignment === 2) {
       enqueueSnackbar("Not supported yet")
     } else {
-      setAlignment(newAlignment);
+      setSearchType(newAlignment);
     }
     
   };
   const handleAlignment2 = (event, newAlignment) => {
-    if(alignment === 2 && newAlignment === 2) {
+    if(searchType === 2 && newAlignment === 2) {
       enqueueSnackbar("Not supported yet")
     } else {
-      setAlignment2(newAlignment);
+      setSearchRule(newAlignment);
     }
   };
 
@@ -39,14 +44,32 @@ export default function SearchPopUp(props) {
     onClose(1);
   };
 
-  const data = [
-    "yes",
-    "no",
-  ]
+  const handleSearchSubmit = (e) => {
+    // Post search only atm
+    e.preventDefault();
+    enqueueSnackbar("Search Submitted");
+    const searchRequestLink = serverUrl + `${searchRule === 2 ? "/posts/searchPostByTag" : "/posts/post"}`;
 
-  const handleListItemClick = (value) => {
-    // onClose(value);
-    // go to a post page or person page (target blank)
+    // send axios search request
+    axios.get(searchRequestLink, {
+      params: {
+        keyword: searchValue
+      }
+    })
+    .then(function (response) {
+      enqueueSnackbar("Search Finished");
+      console.log("response.data: ", response.data);
+      setSearchResult(response.data);
+    })
+    .catch(function (error) {
+      enqueueSnackbar("Search Failed");
+      console.log(error);
+    })
+  }
+
+  const handleListItemClick = (event, id) => {
+    onClose(0);
+    navigate("/post/" + id);
   };
 
   return (
@@ -59,7 +82,7 @@ export default function SearchPopUp(props) {
           alignItems="flex-end"
         >
           <ToggleButtonGroup
-            value={alignment}
+            value={searchType}
             color="primary"
             exclusive
             onChange={handleAlignment}
@@ -75,7 +98,7 @@ export default function SearchPopUp(props) {
 
           <ToggleButtonGroup
             size="small"
-            value={alignment2}
+            value={searchRule}
             exclusive
             onChange={handleAlignment2}
             aria-label="text alignment"
@@ -90,10 +113,7 @@ export default function SearchPopUp(props) {
         </Stack>
 
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log("submitted...")
-          }}
+          onSubmit={handleSearchSubmit}
         >
           <TextField
             label="Search here"
@@ -112,13 +132,16 @@ export default function SearchPopUp(props) {
         <div>
           <List component="nav" aria-label="main mailbox folders">
             {
-              data.map((element,index) => {
+              searchResult.map((element,index) => {
                 return (
                   <ListItemButton
                     key={index}
-                    onClick={(event) => handleListItemClick(event, 0)}
+                    onClick={(event) => handleListItemClick(event, element.id)}
                   >
-                    <ListItemText primary={element} />
+                    <ListItemText 
+                      primary={element.title} 
+                      secondary={element.description}
+                    />
                   </ListItemButton>
                 )
               })
--- server/src/main/java/com/example/server/controller/PostController.java
+++ server/src/main/java/com/example/server/controller/PostController.java
@@ -65,7 +65,7 @@ public class PostController {
         return postService.savePost(postData);
     }
 
-    
+
     /**
      * <p>Delete user from the system based on the ID. The method mapping is like the getUser with difference of
      * @DeleteMapping and @GetMapping</p>
