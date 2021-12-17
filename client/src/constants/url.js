16a945b1033eda4333e7f02a7cc792d43fb97ed0
Zhuowei Zhang <zhuoweiz10@gmail.com>
2021-12-17 05:42:16 -0500

    client deploy

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,5 +1,5 @@
 const dev_url = "http://localhost:8080";
-const deploy_url = ""
-const serverUrl = dev_url;
+const deploy_url = "http://67.205.170.211:8080"
+const serverUrl = deploy_url;
 
 module.exports = { serverUrl };
