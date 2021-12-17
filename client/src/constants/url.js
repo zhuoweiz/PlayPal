5ee291aad93bc00abc991d0b5a380b3ea63266ea
thomasgy422 <thomasku1997@gmail.com>
2021-12-17 12:46:15 -0500

    change

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,5 +1,6 @@
 const dev_url = "http://localhost:8080";
 const deploy_url = "http://67.205.170.211:8080"
-const serverUrl = deploy_url;
+const serverUrl = dev_url;
+// const serverUrl = deploy_url;
 
 module.exports = { serverUrl };
